12bcf8bc53cc7930fcd036b03c0885da85dd38d8
Lucas Fernandes <dev02@neokoros.com>
2022-03-29 17:53:26 -0300

    Criando estrutura para validar links

--- cli.js
+++ cli.js
@@ -1,15 +1,19 @@
 import chalk from 'chalk';
-import getFile from './index.js'
+import getFile from './index.js';
+import validaURLs from './http-validacao.js';
 
 const path = process.argv;
 
 async function processaTexto(path) {
     try{
         const resultado = await getFile(path[2]);
+        if (path[3] === 'validar'){
+            console.log(chalk.yellow('Links validados'), validaURLs(resultado));
+        }
         console.log(chalk.yellow('Lista de links'), resultado)
     } catch(err) {
-        console.log(err)
+        console.log(err);
     }
 }
 
-processaTexto(path)
+processaTexto(path);
