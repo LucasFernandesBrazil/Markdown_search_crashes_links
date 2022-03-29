f0b8d5de76619ae00ea0dc59a192df8f6a4dc174
Lucas Fernandes <dev02@neokoros.com>
2022-03-29 12:32:49 -0300

    Utilização de async await

--- index.js
+++ index.js
@@ -5,22 +5,14 @@ function trataErro(err) {
     throw new Error(chalk.red(err));
 }
 
-// function pegaArquivo (path) {
-//     const encoding = 'utf-8';
-//     fs.readFile(path, encoding, (err, data) => {
-//         if (err) {
-//             trataErro(err);
-//         }
-//         console.log(chalk.green(data));
-//     })
-// }
-
-function pegaArquivo(path) {
+async function pegaArquivo(path) {
     const encoding = 'utf-8';
-    fs.promises
-    .readFile(path, encoding)
-    .then((data) => console.log(chalk.green(data)))
-    .catch((err) => trataErro(err))
+    try{
+        const data = await fs.promises.readFile(path, encoding)
+        console.log(chalk.green(data))
+    }catch (err) {
+        trataErro(err);
+    }
 }
 
 pegaArquivo('./arquivos/texto1.md');
