10f20bf46e374704b71469bbb29a1cc31f4c9290
Lucas Fernandes <dev02@neokoros.com>
2022-03-29 15:12:49 -0300

    Aplicando regex e extraindo links

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
+function extraiLinks(text) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+
+    while ((temp = regex.exec(text)) !== null) {
+        arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados
+}
+
 function trataErro(err) {
     throw new Error(chalk.red(err));
 }
@@ -9,7 +20,7 @@ async function pegaArquivo(path) {
     const encoding = 'utf-8';
     try{
         const data = await fs.promises.readFile(path, encoding)
-        console.log(chalk.green(data))
+        console.log(extraiLinks(data))
     }catch (err) {
         trataErro(err);
     }
