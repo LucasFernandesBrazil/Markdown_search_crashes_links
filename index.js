6ffcb6d9eac765e75e6001a4a4f92df66cc0a4b6
Lucas Fernandes <dev02@neokoros.com>
2022-03-29 12:21:46 -0300

    Aplicando promisses

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import chalk from 'chalk';
 import fs from 'fs';
 
-function trataErro(erro) {
-    throw new Error(chalk.red(erro));
+function trataErro(err) {
+    throw new Error(chalk.red(err));
 }
 
-function pegaArquivo (path) {
-    const encoding = 'utf-8'
-    fs.readFile(path, encoding, (err, data) => {
-        if (err) {
-            trataErro(err);
-        }
-        console.log(chalk.green(data));
-    })
+// function pegaArquivo (path) {
+//     const encoding = 'utf-8';
+//     fs.readFile(path, encoding, (err, data) => {
+//         if (err) {
+//             trataErro(err);
+//         }
+//         console.log(chalk.green(data));
+//     })
+// }
+
+function pegaArquivo(path) {
+    const encoding = 'utf-8';
+    fs.promises.readFile(path, encoding)
+    .then((data) => console.log(chalk.green(data)))
+    .catch((err) => trataErro(err))
 }
 
-pegaArquivo('./arquivos/texto1.md')
+pegaArquivo('./arquivos/texto1.md');
