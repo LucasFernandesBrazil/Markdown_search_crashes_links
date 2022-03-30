afb2b5d354222658473884a30e57bab9391fbe82
Lucas Fernandes <dev02@neokoros.com>
2022-03-30 10:56:51 -0300

    Publicando no npm

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,17 @@
 import pegaArquivo from "../index.js";
 
-test('Deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe('function');
+const arrayResult = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('pegaArquivo::', () => {
+    it('deve ser uma função', () => {
+        expect(typeof pegaArquivo).toBe('function');
+    })
+    // it('deve retornar array com resultados', async () => {
+    //     const result = await pegaArquivo('./arquivos/texto1.md');
+    //     expect(pegaArquivo(result).toEqual(arrayResult));
+    // })
 })
